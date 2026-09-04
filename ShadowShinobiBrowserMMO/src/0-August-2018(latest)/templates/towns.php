7b9ac24f5259a023147b137d06e40982e90a5ae6
Chase <112102369+ChaseChanceChange@users.noreply.github.com>
2026-09-04 23:58:01 +0100

    Modernize town gameplay view

--- ShadowShinobiBrowserMMO/src/0-August-2018(latest)/templates/towns.php
+++ ShadowShinobiBrowserMMO/src/0-August-2018(latest)/templates/towns.php
@@ -1,24 +1,22 @@
 <?php
 $template = <<<THEVERYENDOFYOU
-<table width="100%">
-<tr><td align="center">
-<center><img src="images/town_{{id}}.gif" alt="Bem-vindo à {{name}}" title="Bem-vindo à {{name}}" /></center>
-{{indexconteudo}}
-{{htmlnapag}}
-<br>
-{{fimconteudo}}
-
-</td></tr>
-<tr><td><center>
-{{news}}
-<table width="95%">
-<tr><td width="50%">
-<center>{{whosonline}}</center>
-</td></tr><tr><td>
-{{babblebox}}
-</td></tr>
-</table>
-</td></tr>
-</table>
+<section class="ss-town" aria-labelledby="town-title">
+  <header class="ss-town__hero">
+    <div class="ss-town__art"><img src="images/town_{{id}}.gif" alt="Welcome to {{name}}" title="Welcome to {{name}}"></div>
+    <div class="ss-town__heading">
+      <span class="ss-eyebrow">SAFE ZONE</span>
+      <h2 id="town-title">{{name}}</h2>
+      <p>Rest, resupply, manage your character and interact with other players.</p>
+    </div>
+  </header>
+  {{indexconteudo}}
+  {{htmlnapag}}
+  <div class="ss-town__content">{{fimconteudo}}</div>
+  <div class="ss-town__community">
+    <section class="ss-community-card"><span class="ss-eyebrow">WORLD STATUS</span>{{news}}</section>
+    <section class="ss-community-card"><span class="ss-eyebrow">PLAYERS</span>{{whosonline}}</section>
+    <section class="ss-community-card ss-community-card--chat"><span class="ss-eyebrow">COMMUNICATION</span>{{babblebox}}</section>
+  </div>
+</section>
 THEVERYENDOFYOU;
 ?>
