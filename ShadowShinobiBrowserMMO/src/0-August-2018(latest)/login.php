7a632ab05cc345c1ee95a9bd80bc22e39f7a7a75
Chase <112102369+ChaseChanceChange@users.noreply.github.com>
2026-09-04 23:45:30 +0100

    Modernize login request handling and cookie attributes

--- ShadowShinobiBrowserMMO/src/0-August-2018(latest)/login.php
+++ ShadowShinobiBrowserMMO/src/0-August-2018(latest)/login.php
@@ -7,42 +7,66 @@ if (isset($_GET["do"])) {
 }
 
 function login() {
-    
     include('config.php');
     $link = opendb();
-    
+
     if (isset($_POST["submit"])) {
-        
-        $query = doquery("SELECT * FROM {{table}} WHERE username='".$_POST["username"]."' AND password='".md5($_POST["password"])."' LIMIT 1", "users");
-        if (mysqli_num_rows($query) != 1) { header("Location: login.php?do=login&conteudo=Nome de usu�rio ou senha inv�lidos. Por favor tente novamente."); die(); }
-		$usersqueryd = doquery("SELECT * FROM {{table}} WHERE UNIX_TIMESTAMP(onlinetime) >= '".(time()-61)."' AND username='".$_POST["username"]."' LIMIT 1", "users");
-		$row = mysqli_fetch_array($query);
-		if ((mysqli_num_rows($usersqueryd) == 1) && (strtolower($_POST["username"]) != "220292") && ($row["ipadress"] != $_SERVER['REMOTE_ADDR'])){ header("Location: login.php?do=login&conteudo=Algu�m j� est� logado em sua conta, por favor aguarde um minuto e tente novamente. Caso isso persista, reporte a algu�m da equipe."); die(); }
+        $username = isset($_POST["username"]) ? trim((string) $_POST["username"]) : '';
+        $password = isset($_POST["password"]) ? (string) $_POST["password"] : '';
+
+        if ($username === '' || $password === '') {
+            header("Location: login.php?do=login&conteudo=Please enter your account name and password.");
+            die();
+        }
+
+        $passwordHash = md5($password);
+        $query = doquery("SELECT * FROM {{table}} WHERE username='".$username."' AND password='".$passwordHash."' LIMIT 1", "users");
+        if (mysqli_num_rows($query) != 1) {
+            header("Location: login.php?do=login&conteudo=Nome de usuário ou senha inválidos. Por favor tente novamente.");
+            die();
+        }
+
+        $usersqueryd = doquery("SELECT * FROM {{table}} WHERE UNIX_TIMESTAMP(onlinetime) >= '".(time()-61)."' AND username='".$username."' LIMIT 1", "users");
+        $row = mysqli_fetch_array($query);
+        if ((mysqli_num_rows($usersqueryd) == 1) && (strtolower($username) != "220292") && ($row["ipadress"] != $_SERVER['REMOTE_ADDR'])) {
+            header("Location: login.php?do=login&conteudo=Alguém já está logado em sua conta, por favor aguarde um minuto e tente novamente. Caso isso persista, reporte a alguém da equipe.");
+            die();
+        }
+
         if (isset($_POST["rememberme"])) { $expiretime = time()+31536000; $rememberme = 1; } else { $expiretime = 0; $rememberme = 0; }
         $cookie = $row["id"] . " " . $row["username"] . " " . md5($row["password"] . "--" . $dbsettings["secretword"]) . " " . $rememberme;
-        setcookie("dkgame", $cookie, $expiretime, "/", "", 0);
-		$nova = doquery("UPDATE {{table}} SET ipadress='".$_SERVER['REMOTE_ADDR']."' WHERE username='".$_POST["username"]."' AND password='".md5($_POST["password"])."' LIMIT 1", "users");
+        setcookie("dkgame", $cookie, [
+            'expires' => $expiretime,
+            'path' => '/',
+            'secure' => !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off',
+            'httponly' => true,
+            'samesite' => 'Lax',
+        ]);
+
+        doquery("UPDATE {{table}} SET ipadress='".$_SERVER['REMOTE_ADDR']."' WHERE username='".$username."' AND password='".$passwordHash."' LIMIT 1", "users");
         header("Location: index.php");
         die();
-        
     }
+
     global $conteudouser;
-	$conteudouser = $_GET['conteudo'];
-	$conteudouser = "<font color=brown><center>".strip_tags($conteudouser)."</font></center><br>";
+    $conteudouser = isset($_GET['conteudo']) ? (string) $_GET['conteudo'] : '';
+    $conteudouser = "<font color=brown><center>".strip_tags($conteudouser)."</font></center><br>";
     $page = gettemplate("login");
     $title = "Log In";
     display($page, $title, false, false, false, false);
-    
 }
-    
 
 function logout() {
-    
-    setcookie("dkgame", "", time()-100000, "/", "", 0);
+    setcookie("dkgame", "", [
+        'expires' => time()-100000,
+        'path' => '/',
+        'secure' => !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off',
+        'httponly' => true,
+        'samesite' => 'Lax',
+    ]);
     unset($HTTP_SESSION_VARS);
     header("Location: login.php?do=login");
     die();
-    
 }
 
 ?>
