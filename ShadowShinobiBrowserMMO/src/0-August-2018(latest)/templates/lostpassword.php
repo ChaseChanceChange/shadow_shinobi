4f3f0cc39560aed6950c730432bd238aa71692f9
Chase <112102369+ChaseChanceChange@users.noreply.github.com>
2026-09-04 23:53:07 +0100

    Modernize password recovery form

--- ShadowShinobiBrowserMMO/src/0-August-2018(latest)/templates/lostpassword.php
+++ ShadowShinobiBrowserMMO/src/0-August-2018(latest)/templates/lostpassword.php
@@ -1,15 +1,23 @@
 <?php
 global $conteudouser;
 $template = <<<THEVERYENDOFYOU
-<table width="100%"><tr><td width="100%" align="center"><center><img src="images/minhaconta.gif" /></center></td></tr></table>$conteudouser
-<form action="users.php?do=lostpassword" method="post" id="formback"><fieldset id="field2"><legend>Recuperar Senha</legend>
-<table width="80%">
-<tr><td colspan="2">Se você perdeu sua senha, entre com seu e-mail abaixo e você receberá uma nova.</td></tr>
-<tr><td width="20%">Endereço de E-mail:</td><td><input type="text" name="email" size="30" maxlength="100" /></td></tr>
-<tr><td colspan="2"><center><div class="buttons"><button type="submit" class="positive" name="submit"><img src="layoutnovo/dropmenu/b1.gif"> Enviar Senha</button>
-<button type="reset" class="negative" name="reset"><img src="layoutnovo/dropmenu/b3.gif"> Resetar</button>
-</div></center></td></tr>
-</table></fieldset>
-</form>
+<section class="ss-card ss-account-form">
+  <div class="ss-action-card__header">
+    <span class="ss-eyebrow">ACCOUNT RECOVERY</span>
+    <h2>Recover your password</h2>
+    $conteudouser
+    <p>Enter the email address associated with your account to begin recovery.</p>
+  </div>
+  <form action="users.php?do=lostpassword" method="post" class="ss-account-form__fields">
+    <div class="ss-form-field">
+      <label for="recovery-email">Email address</label>
+      <input id="recovery-email" type="email" name="email" size="30" maxlength="100" autocomplete="email" required />
+    </div>
+    <div class="buttons ss-action-card__actions">
+      <button type="submit" class="positive" name="submit"><img src="layoutnovo/dropmenu/b1.gif" alt=""> Send recovery</button>
+      <button type="reset" class="negative" name="reset"><img src="layoutnovo/dropmenu/b3.gif" alt=""> Reset</button>
+    </div>
+  </form>
+</section>
 THEVERYENDOFYOU;
 ?>
