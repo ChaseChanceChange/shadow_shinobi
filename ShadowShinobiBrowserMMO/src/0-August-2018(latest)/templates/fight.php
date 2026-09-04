1ff932c7c590c9e4cfd9ead7a6cc8998acf0b8e0
Chase <112102369+ChaseChanceChange@users.noreply.github.com>
2026-09-04 23:51:51 +0100

    Modernize combat presentation without changing combat logic

--- ShadowShinobiBrowserMMO/src/0-August-2018(latest)/templates/fight.php
+++ ShadowShinobiBrowserMMO/src/0-August-2018(latest)/templates/fight.php
@@ -1,28 +1,33 @@
 <?php
 $template = <<<THEVERYENDOFYOU
-<table width="100%">
-<tr><td align="center"><center><img src="images/title_fighting.gif" alt="Fighting" /></center>{{indexconteudo}}</td></tr>
-<tr><td>
-<div style="position: relative; top: -15px; margin-bottom: -30px;">
-<table><tr><td width="310"><br>
-{{dados}}
-<center>Você está lutando com: <b>{{monstername}}</b>.<br /><br></center>
-{{yourturn}}
-{{monsterturn}}
-{{monsterhp}}
-{{command}}
-</td><td>
+<section class="ss-combat" aria-labelledby="ss-combat-title">
+  <div class="ss-combat__header">
+    <div>
+      <h1 class="ss-combat__title" id="ss-combat-title">Battle</h1>
+      <div class="ss-combat__sub">Combat results and commands are driven by the legacy game engine.</div>
+    </div>
+    <div class="small">{{monstername}}</div>
+  </div>
 
+  {{indexconteudo}}
 
-
-<table width="165" height="175" background="layoutnovo/graficos/fundo.png" style="background-repeat:no-repeat;;background-position:left top"><tr height="{{porcent}}"><td></td></tr><tr ><td valign="bottom"><center><img src="layoutnovo/graficos/{{grafico}}"></center>
-</td></tr><tr><td></td></tr></table>
-
-
-
-</td></tr></table>
-</div>
-</td></tr>
-</table>
+  <div class="ss-combat__arena">
+    <div class="ss-combat__panel">
+      <div class="ss-combat__fighter">
+        <div>
+          {{dados}}
+          <p><strong>You are fighting:</strong> {{monstername}}</p>
+          {{yourturn}}
+          {{monsterturn}}
+          {{monsterhp}}
+          {{command}}
+        </div>
+        <div class="ss-combat__graphic" aria-label="Battle illustration">
+          <table width="165" height="175" background="layoutnovo/graficos/fundo.png" style="background-repeat:no-repeat;background-position:left top"><tr height="{{porcent}}"><td></td></tr><tr><td valign="bottom"><center><img src="layoutnovo/graficos/{{grafico}}" alt="Battle opponent" /></center></td></tr><tr><td></td></tr></table>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 THEVERYENDOFYOU;
 ?>
