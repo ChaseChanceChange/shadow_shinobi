65cf6e51a83c60d9205333d3285d87b93b587fa5
Chase <112102369+ChaseChanceChange@users.noreply.github.com>
2026-09-06 00:34:53 +0100

    retheme: convert combat presentation to Shadow terminology

--- ShadowShinobiBrowserMMO/src/0-August-2018(latest)/templates/fight.php
+++ ShadowShinobiBrowserMMO/src/0-August-2018(latest)/templates/fight.php
@@ -3,7 +3,7 @@ $template = <<<THEVERYENDOFYOU
 <section class="ss-combat" aria-labelledby="ss-combat-title">
   <div class="ss-combat__header">
     <div>
-      <h1 class="ss-combat__title" id="ss-combat-title">Battle</h1>
+      <h1 class="ss-combat__title" id="ss-combat-title">Combat</h1>
       <div class="ss-combat__sub">Combat results and commands are driven by the legacy game engine.</div>
     </div>
     <div class="small">{{monstername}}</div>
@@ -16,14 +16,14 @@ $template = <<<THEVERYENDOFYOU
       <div class="ss-combat__fighter">
         <div>
           {{dados}}
-          <p><strong>You are fighting:</strong> {{monstername}}</p>
+          <p><strong>Current Threat:</strong> {{monstername}}</p>
           {{yourturn}}
           {{monsterturn}}
           {{monsterhp}}
           {{command}}
         </div>
-        <div class="ss-combat__graphic" aria-label="Battle illustration">
-          <table width="165" height="175" background="layoutnovo/graficos/fundo.png" style="background-repeat:no-repeat;background-position:left top"><tr height="{{porcent}}"><td></td></tr><tr><td valign="bottom"><center><img src="layoutnovo/graficos/{{grafico}}" alt="Battle opponent" /></center></td></tr><tr><td></td></tr></table>
+        <div class="ss-combat__graphic" aria-label="Combat illustration">
+          <table width="165" height="175" background="layoutnovo/graficos/fundo.png" style="background-repeat:no-repeat;background-position:left top"><tr height="{{porcent}}"><td></td></tr><tr><td valign="bottom"><center><img src="layoutnovo/graficos/{{grafico}}" alt="Combat opponent" /></center></td></tr><tr><td></td></tr></table>
         </div>
       </div>
     </div>
