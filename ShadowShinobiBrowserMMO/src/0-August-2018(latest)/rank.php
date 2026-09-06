9878c2ed610eb0f97847de2ce06793e0524fb8c3
Chase <112102369+ChaseChanceChange@users.noreply.github.com>
2026-09-06 05:06:29 +0100

    content: retheme player rankings page

--- ShadowShinobiBrowserMMO/src/0-August-2018(latest)/rank.php
+++ ShadowShinobiBrowserMMO/src/0-August-2018(latest)/rank.php
@@ -1,7 +1,7 @@
-<?php 
-include('lib.php'); 
+<?php
+include('lib.php');
 $link = opendb();
-$controlquery = doquery("SELECT * FROM {{table}} WHERE id='1' LIMIT 1", "control");
+$controlquery = doquery("SELECT * FROM {{table}} WHERE id='1' LIMIT 1", 'control');
 $controlrow = mysqli_fetch_array($controlquery) ?: array();
 
 include('cookies.php');
@@ -13,30 +13,35 @@ if (!in_array($ord, $allowedOrders, true)) {
     $ord = 'level';
 }
 
-$page = "
-<table width=\"100%\"><tr><td width=\"100%\" align=\"center\"><center><img src=\"images/rank.gif\" /></center></td></tr></table>
-<center>[ <a href=\"index.php\">Voltar ao Jogo</a> ]</center>
+$labels = array(
+    'level' => 'Standing',
+    'attackpower' => 'Attack Power',
+    'defensepower' => 'Defense Power',
+);
 
-<br />
-
-<table width=\"100%\" style=\"border: solid 1px black\" cellspacing=\"0\" cellpadding=\"0\">
-<tr><td colspan=\"5\" bgcolor=\"#ffffff\"><center><b>Rank por Level</b></center></td></tr>
-<tr><td><b>Rank</b></td><td><b><a href=\"rank.php?ord=level\" title=\"Ordenar por Level\">Level</a></b></td><td><a href=\"rank.php?ord=attackpower\" title=\"Ordenar por Poder de Ataque\">Poder de Ataque</a></td><td><a href=\"rank.php?ord=defensepower\" title=\"Ordenar por Poder de Defesa\">Poder de Defesa</a></td><td><b>Nome</b></td></tr>
-";
+$page = '<section class="ss-card ss-rankings">'
+    . '<div class="ss-action-card__header"><span class="ss-eyebrow">SHADOW SHINOBI</span><h2>Standing Rankings</h2>'
+    . '<p>Track the field by progression, offensive power and defensive power.</p></div>'
+    . '<div class="ss-ranking-toolbar">'
+    . '<a href="rank.php?ord=level">Standing</a>'
+    . '<a href="rank.php?ord=attackpower">Attack Power</a>'
+    . '<a href="rank.php?ord=defensepower">Defense Power</a>'
+    . '</div>'
+    . '<table class="ss-data-table"><thead><tr><th>Place</th><th>' . $labels[$ord] . '</th><th>Attack Power</th><th>Defense Power</th><th>Operative</th></tr></thead><tbody>';
 
 $count = 1;
-$contagemrank = 0;
-$usersquery = doquery("SELECT * FROM {{table}} ORDER BY $ord DESC, level DESC LIMIT 103", "users");
+$rankPosition = 0;
+$usersquery = doquery("SELECT * FROM {{table}} ORDER BY $ord DESC, level DESC LIMIT 103", 'users');
 while ($usersrow = mysqli_fetch_array($usersquery)) {
-    if (($usersrow["authlevel"] ?? 0) != 1) {
-        if ($count == 1) { $color = "bgcolor=\"#ffffff\""; $count = 2; } else { $color = ""; $count = 1; }
-        $contagemrank += 1;
-        $charname = htmlspecialchars((string)($usersrow["charname"] ?? ''), ENT_QUOTES, 'UTF-8');
-        $page .= "<tr><td $color width=\"15%\">$contagemrank</td><td $color width=\"15%\">".($usersrow["level"] ?? 0)."</td><td $color width=\"15%\">".($usersrow["attackpower"] ?? 0)."</td><td $color width=\"15%\">".($usersrow["defensepower"] ?? 0)."</td><td $color width=\"*\"><a href=\"javascript: mostrarchar('".$charname."');\">".$charname."</a></td></tr>\n";
+    if (($usersrow['authlevel'] ?? 0) != 1) {
+        $rankPosition++;
+        $charname = htmlspecialchars((string)($usersrow['charname'] ?? ''), ENT_QUOTES, 'UTF-8');
+        $page .= '<tr><td>' . $rankPosition . '</td><td>' . (int)($usersrow['level'] ?? 0) . '</td><td>'
+            . (int)($usersrow['attackpower'] ?? 0) . '</td><td>' . (int)($usersrow['defensepower'] ?? 0) . '</td><td>'
+            . '<a href="javascript:mostrarchar(\'' . addslashes($charname) . '\');">' . $charname . '</a></td></tr>';
     }
 }
 
-$page .= "
-</table>";
-display($page, "Rank por Level", false, false, false);
+$page .= '</tbody></table></section>';
+display($page, 'Standing Rankings', false, false, false);
 ?>
